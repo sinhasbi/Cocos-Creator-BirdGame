34c1fc97ea2d20d403b3ece865a1c97b1d73f1d5
hasbi <sinhasbi123@gmail.com>
2025-03-05 17:25:34 +0800

    Ground Infinty Animation

--- assets/Scences/Ground.ts
+++ assets/Scences/Ground.ts
@@ -1,14 +1,89 @@
-import { _decorator, Component, Node } from 'cc';
+import {
+  _decorator,
+  Component,
+  Node,
+  Vec3,
+  UITransform,
+  director,
+  Canvas,
+} from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('NewComponent')
-export class NewComponent extends Component {
-    start() {
+@ccclass("Ground")
+export class Ground extends Component {
+  @property({
+    type: Node,
+    tooltip: "Ground 1 is here",
+  })
+  public ground1: Node;
+  @property({
+    type: Node,
+    tooltip: "Ground 2 is here",
+  })
+  public ground2: Node = null;
 
+  @property({
+    type: Node,
+    tooltip: "Ground 3 is here",
+  })
+  public ground3: Node = null;
+
+  //Create ground with varibales
+  public groundWidth1: number;
+  public groundWidth2: number;
+  public groundWidth3: number;
+
+  public tempStartLocation1 = new Vec3();
+  public tempStartLocation2 = new Vec3();
+  public tempStartLocation3 = new Vec3();
+
+  public gameSpeed: number = 100;
+
+  onLoad() {
+    this.startup();
+  }
+
+  startup() {
+    this.groundWidth1 = this.ground1.getComponent(UITransform).width;
+    this.groundWidth2 = this.ground2.getComponent(UITransform).width;
+    this.groundWidth3 = this.ground3.getComponent(UITransform).width;
+
+    this.tempStartLocation1.x = 0;
+    this.tempStartLocation2.x = this.groundWidth1;
+    this.tempStartLocation3.x = this.groundWidth1 + this.groundWidth2;
+
+    this.ground1.setPosition(this.tempStartLocation1);
+    this.ground2.setPosition(this.tempStartLocation2);
+    this.ground3.setPosition(this.tempStartLocation3);
+  }
+
+
+  update(deltaTime: number) {
+    this.tempStartLocation1 = this.ground1.position;
+    this.tempStartLocation2 = this.ground2.position;
+    this.tempStartLocation3 = this.ground3.position;
+
+    this.tempStartLocation1.x -= this.gameSpeed * deltaTime;
+    this.tempStartLocation2.x -= this.gameSpeed * deltaTime;
+    this.tempStartLocation3.x -= this.gameSpeed * deltaTime;
+
+    const scene = director.getScene();
+    const canvas = scene.getComponentsInChildren(Canvas);
+
+    if (this.tempStartLocation1.x <= 0 - this.groundWidth1) {
+      this.tempStartLocation1.x = canvas[0].getComponent(UITransform).width;
     }
 
-    update(deltaTime: number) {
-        
+    if (this.tempStartLocation2.x <= 0 - this.groundWidth2) {
+      this.tempStartLocation2.x = canvas[0].getComponent(UITransform).width;
     }
+
+    if (this.tempStartLocation3.x <= 0 - this.groundWidth3) {
+      this.tempStartLocation3.x = canvas[0].getComponent(UITransform).width;
+    }
+
+    this.ground1.setPosition(this.tempStartLocation1);
+    this.ground2.setPosition(this.tempStartLocation2);
+    this.ground3.setPosition(this.tempStartLocation3);
+  }
 }
-
